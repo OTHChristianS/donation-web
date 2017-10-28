75adbf45284aa8a8ea47f9ecf1ebf4e72311c85b
OTHChristianS <christian.schuetze@st.oth-regensburg.de>
2017-10-28 16:26:39 +0200

    Lab6 Exercise Step 3: Added Views

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -3,23 +3,23 @@
 exports.home = {
 
     handler: (request, reply) => {
-        reply.file('./app/views/main.html');
-    }
+        reply.view('main', {title: 'Welcome to Donations'});
+    },
 
 };
 
 exports.signup = {
 
     handler: (request, reply) => {
-        reply.file('./app/views/signup.html');
-    }
+        reply.view('signup', {title: 'Sign up for Donations'});
+    },
 
 };
 
 exports.login = {
 
     handler: (request, reply) => {
-        reply.file('./app/views/login.html');
-    }
+        reply.view('login', {title: 'Login to Donations'});
+    },
 
 };
