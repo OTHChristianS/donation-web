02b7f9b0f8e569fd2864c725732c5378ed3989c2
OTHChristianS <christian.schuetze@st.oth-regensburg.de>
2017-11-04 11:16:00 +0100

    Lab11 Step 5: Added candidates to the donation report list

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -10,28 +10,26 @@ exports.home = {
         Candidate.find({}).then(candidates => {
             reply.view('home', {
                 title: 'Make a Donation',
-                candidates: candidates,
+                candidates: candidates
             });
         }).catch(err => {
             reply.redirect('/');
         });
-    },
-
+    }
 };
 
 exports.report = {
 
     handler: function (request, reply) {
-        Donation.find({}).populate('donor').then(allDonations => {
+        Donation.find({}).populate('donor').populate('candidate').then(allDonations => {
             reply.view('report', {
                 title: 'Donations to Date',
-                donations: allDonations,
+                donations: allDonations
             });
         }).catch(err => {
             reply.redirect('/');
         });
-    },
-
+    }
 };
 
 exports.donate = {
@@ -44,7 +42,7 @@ exports.donate = {
             const rawCandidate = request.payload.candidate.split(',');
             Candidate.findOne({
                 lastName: rawCandidate[0],
-                firstName: rawCandidate[1],
+                firstName: rawCandidate[1]
             }).then(candidate => {
                 donation.donor = user._id;
                 donation.candidate = candidate._id;
