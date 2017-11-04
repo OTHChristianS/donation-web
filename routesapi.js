e474f70785657b0bd0b0f4c69bb0c7c10a0210ee
OTHChristianS <christian.schuetze@st.oth-regensburg.de>
2017-11-04 12:10:50 +0100

    Lab12 Step 4: Added REST API to receive a candidate by id

--- routesapi.js
+++ routesapi.js
@@ -1,5 +1,6 @@
 const CandidatesApi = require('./app/api/candidatesapi');
 
 module.exports = [
+    {method: 'GET', path: '/api/candidates/{id}', config: CandidatesApi.findOne},
     {method: 'GET', path: '/api/candidates', config: CandidatesApi.find}
 ];
